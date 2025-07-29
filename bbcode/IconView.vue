freenutsxd/Horizon

commit 103d53a1909cd16eb8b4323785165f2ec90dabf3

Fixes right clicking on [icon] tags not opening the context menu anymore
Closes #157

bbcode/IconView.vue
@@ -2,6 +2,7 @@
   <a
     :href="`${Utils.siteDomain}c/${character.name}`"
     target="_blank"
+    :data-character="character.name"
     @mouseover.prevent="show()"
     @mouseenter.prevent="show()"
     @mouseleave.prevent="dismiss()"

commit a9fd3056dfa77145aa4324005b7c69d8e89ed7d4

Fixes right click for [icon] tags, but without breaking left click
The implementation in 103d53a broke the url link in the Vue component.

Closes #157

bbcode/IconView.vue
@@ -2,7 +2,7 @@
   <a
     :href="`${Utils.siteDomain}c/${character.name}`"
     target="_blank"
-    :data-character="character.name"
+    v-bind:character.prop="character"
     @mouseover.prevent="show()"
     @mouseenter.prevent="show()"
     @mouseleave.prevent="dismiss()"