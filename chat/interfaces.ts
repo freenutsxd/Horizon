5fe025a0e5e9edd36d09e7540ce90b3af983c2a7
Matthew-X <sshadow.230701@gmail.com>
2025-10-25 17:33:47 +1300

    feat: integrate AvailableSort type into member filters for improved sorting options

--- chat/interfaces.ts
+++ chat/interfaces.ts
@@ -228,6 +228,8 @@ export interface ExtendedSearchData extends SearchData {
   species: SearchSpecies[];
 }
 
+export type AvailableSort = 'normal' | 'status' | 'gender' | 'compatibility';
+
 export namespace Settings {
   export type Keys = {
     settings: Settings;
@@ -299,7 +301,7 @@ export namespace Settings {
     readonly horizonShowGenderMarker: boolean;
     readonly horizonAutoGenderFilter: boolean;
     readonly horizonSavedGenderFilters: ReadonlyArray<string>;
-    readonly horizonSavedMembersSort: string;
+    readonly horizonSavedMembersSort: AvailableSort;
     readonly horizonPersistentMemberFilters: boolean;
     readonly horizonGenderMarkerOrigColor: boolean;
     readonly horizonChangeOfflineColor: boolean;
