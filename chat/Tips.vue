4559f91c2977f4a6a7bf64ab103780b3bfb0db2f
FatCat <82377182+FatCatClient@users.noreply.github.com>
2025-11-08 10:30:40 +0100

    Moves tips to bespoke component, adds scrolling and dismiss functionality

---
+++ chat/Tips.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="alert alert-info align-items-center d-flex" v-if="show">
+    <div class="flex-grow-1">
+      <b> {{ l('tips') + ': ' }}</b>
+      <span>
+        {{ tip() }}
+      </span>
+    </div>
+    <div class="btn-group flex-shrink-0">
+      <button type="button" class="btn btn-link" @click="handlePrevious()">
+        <i class="fa-solid fa-arrow-left"></i>
+      </button>
+      <button type="button" class="btn btn-link" @click="handleNext()">
+        <i class="fa-solid fa-arrow-right"></i>
+      </button>
+      <button type="button" class="btn btn-link" @click="show = !show">
+        <i class="fa-solid fa-xmark"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { defineComponent, ref, watch, nextTick } from 'vue';
+  import tip, { previousTip, nextTip } from './tips';
+  import l from './localize';
+  export default defineComponent({
+    setup(props) {
+      const show = ref(true);
+
+      function handleNext() {
+        console.log(nextTip());
+      }
+
+      function handlePrevious() {
+        console.log(previousTip());
+      }
+      return {
+        show,
+        handleNext,
+        handlePrevious,
+        l,
+        tip
+      };
+    }
+  });
+</script>
