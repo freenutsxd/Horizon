8281c98dc98c02873cb33667eb6a97318b4f2cbe
Sylvia Roselie <rose@rosely.me>
2025-11-14 18:46:45 -0500

    refactor(convert): character_page/images.vue → Comp API
    Converts `character_page/images.vue` to the composition API.
    Closes #492
    
    Authored by:
    Rose (rose@rosely.me)

--- site/character_page/images.vue
+++ site/character_page/images.vue
@@ -29,10 +29,9 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
   import log from 'electron-log'; //tslint:disable-line:match-default-export-name
-  import { Component, Prop } from '@f-list/vue-ts';
-  import Vue from 'vue';
+  import { ref } from 'vue';
   import { CharacterImage } from '../../interfaces';
   import * as Utils from '../utils';
   import { methods } from './data_store';
@@ -41,118 +40,114 @@
   import _ from 'lodash';
   import l from '../../chat/localize';
 
-  @Component
-  export default class ImagesView extends Vue {
-    @Prop({ required: true })
-    private readonly character!: Character;
-    @Prop
-    private readonly usePreview?: boolean;
-    @Prop
-    private readonly injectedImages?: CharacterImage[] | null;
+  const props = defineProps<{
+    character: Character;
+    usePreview?: boolean;
+    injectedImages?: CharacterImage[] | null;
+  }>();
 
-    private shown = false;
-    previewImage = '';
-    images: CharacterImage[] = [];
-    loading = true;
-    error = '';
-    l = l;
+  const shown = ref(false);
+  const previewImage = ref('');
+  const images = ref<CharacterImage[]>([]);
+  const loading = ref(true);
+  const error = ref('');
 
-    imageUrl = (image: CharacterImage) => methods.imageUrl(image);
-    thumbUrl = (image: CharacterImage) => methods.imageThumbUrl(image);
+  const imageUrl = (image: CharacterImage) => methods.imageUrl(image);
+  const thumbUrl = (image: CharacterImage) => methods.imageThumbUrl(image);
 
-    show(): void {
-      log.debug('profile.images.show', {
-        shown: this.shown,
-        loading: this.loading
-      });
+  const resolveImages = (): CharacterImage[] => {
+    log.debug('profile.images.sync.injected', {
+      count: props.injectedImages ? props.injectedImages.length : 0
+    });
 
-      if (this.shown) {
-        return;
-      }
-
-      this.images = this.resolveImages();
-
-      // this promise is intentionally not part of a chain
-      this.showAsync().catch(err => log.error('profile.images.error', { err }));
+    if (props.injectedImages && props.injectedImages.length) {
+      return props.injectedImages;
     }
 
-    async showAsync(): Promise<void> {
-      log.debug('profile.images.show.async', {
-        shown: this.shown,
-        loading: this.loading
-      });
+    const c = core.cache.profileCache.getSync(props.character.character.name);
 
-      if (this.shown) return;
-      try {
-        this.error = '';
-        this.shown = true;
-        this.loading = true;
-        this.images = await this.resolveImagesAsync();
-      } catch (err) {
-        this.shown = false;
-        if (Utils.isJSONError(err))
-          this.error = <string>err.response.data.error;
-        Utils.ajaxError(err, l('profile.images.unableRefresh'));
-        log.error('profile.images.show.async.error', { err });
-      }
-      this.loading = false;
+    log.debug('profile.images.sync.cache', {
+      count: _.get(c, 'meta.images.length')
+    });
+
+    if (c && c.meta && c.meta.images) {
+      return c.meta.images;
     }
 
-    async resolveImagesAsync(): Promise<CharacterImage[]> {
-      log.debug('profile.images.async.injected', {
-        count: this.injectedImages ? this.injectedImages.length : 0
-      });
+    return [];
+  };
 
-      if (this.injectedImages && this.injectedImages.length) {
-        return this.injectedImages;
-      }
+  const resolveImagesAsync = async (): Promise<CharacterImage[]> => {
+    log.debug('profile.images.async.injected', {
+      count: props.injectedImages ? props.injectedImages.length : 0
+    });
 
-      const c = await core.cache.profileCache.get(
-        this.character.character.name
-      );
-
-      log.debug('profile.images.async.cache', {
-        count: _.get(c, 'meta.images.length')
-      });
-
-      if (c && c.meta && c.meta.images) {
-        return c.meta.images;
-      }
-
-      const images = await methods.imagesGet(this.character.character.id);
-
-      log.debug('profile.images.async.api', { count: images.length });
-
-      return images;
+    if (props.injectedImages && props.injectedImages.length) {
+      return props.injectedImages;
     }
 
-    resolveImages(): CharacterImage[] {
-      log.debug('profile.images.sync.injected', {
-        count: this.injectedImages ? this.injectedImages.length : 0
-      });
+    const c = await core.cache.profileCache.get(props.character.character.name);
 
-      if (this.injectedImages && this.injectedImages.length) {
-        return this.injectedImages;
-      }
+    log.debug('profile.images.async.cache', {
+      count: _.get(c, 'meta.images.length')
+    });
 
-      const c = core.cache.profileCache.getSync(this.character.character.name);
-
-      log.debug('profile.images.sync.cache', {
-        count: _.get(c, 'meta.images.length')
-      });
-
-      if (c && c.meta && c.meta.images) {
-        return c.meta.images;
-      }
-
-      return [];
+    if (c && c.meta && c.meta.images) {
+      return c.meta.images;
     }
 
-    handleImageClick(e: MouseEvent, image: CharacterImage): void {
-      if (this.usePreview) {
-        this.previewImage = methods.imageUrl(image);
-        e.preventDefault();
-      }
+    const fetchedImages = await methods.imagesGet(props.character.character.id);
+
+    log.debug('profile.images.async.api', { count: fetchedImages.length });
+
+    return fetchedImages;
+  };
+
+  const showAsync = async (): Promise<void> => {
+    log.debug('profile.images.show.async', {
+      shown: shown.value,
+      loading: loading.value
+    });
+
+    if (shown.value) return;
+    try {
+      error.value = '';
+      shown.value = true;
+      loading.value = true;
+      images.value = await resolveImagesAsync();
+    } catch (err) {
+      shown.value = false;
+      if (Utils.isJSONError(err)) error.value = <string>err.response.data.error;
+      Utils.ajaxError(err, l('profile.images.unableRefresh'));
+      log.error('profile.images.show.async.error', { err });
     }
-  }
+    loading.value = false;
+  };
+
+  const show = (): void => {
+    log.debug('profile.images.show', {
+      shown: shown.value,
+      loading: loading.value
+    });
+
+    if (shown.value) {
+      return;
+    }
+
+    images.value = resolveImages();
+
+    // this promise is intentionally not part of a chain
+    showAsync().catch(err => log.error('profile.images.error', { err }));
+  };
+
+  const handleImageClick = (e: MouseEvent, image: CharacterImage): void => {
+    if (props.usePreview) {
+      previewImage.value = methods.imageUrl(image);
+      e.preventDefault();
+    }
+  };
+
+  defineExpose({
+    show
+  });
 </script>
