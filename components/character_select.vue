4f0d4dac897096dee89818172a0518c400695745
AriannaAltomare <31604240+AriannaAltomare@users.noreply.github.com>
2025-12-20 08:09:27 -0800

    Convert character_select.vue to Composition API (#553)
    * Convert character_select.vue to Composition API
    
    * Rename default (should be modalValue, it seems)
    
    * Restore var name to fix v-model binding
    
    * Support Vue 2 defaults

--- components/character_select.vue
+++ components/character_select.vue
@@ -1,29 +1,50 @@
 <template>
-  <select :value="value" @change="emit" class="form-select">
-    <option v-for="character in characters" :value="character.id">
+  <select :value="selected" @change="onSelectChange" class="form-select">
+    <option
+      v-for="character in characters"
+      :key="character.id"
+      :value="character.id"
+    >
       {{ character.name }}
     </option>
     <slot></slot>
   </select>
 </template>
 
-<script lang="ts">
-  import { Component, Prop } from '@f-list/vue-ts';
-  import Vue from 'vue';
+<script setup lang="ts">
+  import { computed } from 'vue';
   import { SimpleCharacter } from '../interfaces';
   import * as Utils from '../site/utils';
 
-  @Component
-  export default class CharacterSelect extends Vue {
-    @Prop({ required: true })
-    readonly value!: number;
+  const props = defineProps<{
+    // Maintaining Vue 2 backwards-compat, remove after full Vue 3 migration
+    value: number;
 
-    get characters(): SimpleCharacter[] {
-      return Utils.characters;
-    }
+    // Forward-compat for Vue 3
+    modelValue: number;
+  }>();
 
-    emit(evt: Event): void {
-      this.$emit('input', parseInt((<HTMLSelectElement>evt.target).value, 10));
-    }
-  }
+  const emit = defineEmits<{
+    // Maintaining Vue 2 backwards-compat, remove after full Vue 3 migration
+    (e: 'input', value: number): void;
+
+    // Forward-compat for Vue 3
+    (e: 'update:modelValue', value: number): void;
+  }>();
+
+  const characters = computed<SimpleCharacter[]>(() => Utils.characters);
+  const selected = computed<number>(() => {
+    return props.modelValue !== undefined ? props.modelValue : props.value;
+  });
+
+  const onSelectChange = (evt: Event): void => {
+    const target = evt.target as HTMLSelectElement;
+    const newValue = parseInt(target.value, 10);
+
+    // Maintaining Vue 2 backwards-compat, remove after full Vue 3 migration
+    emit('input', newValue);
+
+    // Forward-compat for Vue 3
+    emit('update:modelValue', newValue);
+  };
 </script>
